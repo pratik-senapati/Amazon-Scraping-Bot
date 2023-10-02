be7d38624775731528e6287fd9ab0c6c07976f72
Pratik Senapati <90540195+CriMsonTYpo@users.noreply.github.com>
2023-10-02 16:51:15 +0530

    handles 28/32 main departments

--- amazon/venv/amazon_scraper/amazon_scraper/spiders/amazonSpider.py
+++ amazon/venv/amazon_scraper/amazon_scraper/spiders/amazonSpider.py
@@ -1,28 +1,49 @@
 import scrapy
 import datetime
+from urllib.parse import urljoin
+
+
 
 
 class AmazonspiderSpider(scrapy.Spider):
     name = "amazonSpider"
     allowed_domains = ["www.amazon.in"]
-    start_urls = ["https://www.amazon.in/gp/bestsellers/grocery/4859478031/ref=zg_bs_nav_grocery_1"]
-
+    start_urls = ["https://www.amazon.in/gp/bestsellers"]
+    
     
     def parse(self, response):
+        treeitem_divs = response.css('div[role="treeitem"] a::attr(href)').getall()
+        for rel_url in treeitem_divs:
+            base_url = 'https://www.amazon.in'
+            absolute_url =base_url+rel_url
+            yield response.follow(absolute_url, callback=self.parse_page)
+            
+            
         
+        
+    
+    
+    def parse_page(self, response):
         items = response.css('#gridItemRoot')[:10]
+        department=response.xpath('/html/body/div[1]/div[2]/div/div/div[1]/div/div/div[1]/h1/text()').get().split(' in ')[1]
         count=1
         for item in items:
             name = item.xpath(f'//*[@id="p13n-asin-index-{count}"]/div[2]/div/a/span/div/text()').get().strip()
-            stars = item.xpath(f'//*[@id="p13n-asin-index-{count}"]/div[2]/div/div/div/a/@title').get()
+            stars = item.xpath(f'//*[@id="p13n-asin-index-{count}"]/div[2]/div/div/div/a/@title').get().split(' ')[0]
             reviews = item.xpath(f'//*[@id="p13n-asin-index-{count}"]/div[2]/div/div/div/a/span/text()').get()
+            item_id=item.xpath(f'//*[@id="p13n-asin-index-{count}"]/div[2]/div/@id').get().strip()
 
+            
             yield {
+                'item_id':item_id,
+                'department':department,
                 'name': name,
                 'stars': stars.strip() if stars else None,
                 'reviews': reviews.strip() if reviews else None,
                 'rank':count,
-                'date': datetime.datetime.now().date()
+                'date': datetime.datetime.now().date(),
             }
 
             count+=1
+    
+        
